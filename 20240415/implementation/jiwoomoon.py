91d50926a76cf3aa6d3d3f640b4ede8f48a79e1a
easywoo8080 <easywoo8080@gmail.com>
2024-04-23 18:25:21 +0900

    문지우 재업로드

--- 20240415/implementation/jiwoomoon.py
+++ 20240415/implementation/jiwoomoon.py
@@ -10,27 +10,50 @@ n=17일때 까지 피보나치 수를 써보면 다음과 같다.
 n이 주어졌을 때, n번째 피보나치 수를 구하는 프로그램을 작성하시오.
 
 """
+#
+# def fibo_fn(a, b, n):
+#     if n <= 0:
+#         return [0]
+#     elif n == 1:
+#         return [a]
+#     elif n == 2:
+#         return [a, b]
+#     else:
+#         fibo = [a, b]
+#         for i in range(n - 1):
+#             next_fib = fibo[-1] + fibo[-2]
+#             fibo.append(next_fib)
+#
+#         return fibo
+#
+# # 시작값 a와 b, 그리고 피보나치 수열의 길이 n
+# a = 0
+# b = 1
+# n = int(input())
+#
+# result = fibo_fn(a, b, n)
+#
+# print( result[-1]
+#
+#
+# def fibonacci(n):
+#     if n <= 1:
+#         return n
+#
+#     prev = 0
+#     curr = 1
+#     for _ in range(2, n + 1):
+#         prev, curr = curr, prev + curr
+#
+#     return curr
 
-def fibo_fn(a, b, n):
-    if n <= 0:
-        return [0]
-    elif n == 1:
-        return [a]
-    elif n == 2:
-        return [a, b]
-    else:
-        fibo = [a, b]
-        for i in range(n - 1):
-            next_fib = fibo[-1] + fibo[-2]
-            fibo.append(next_fib)
 
-        return fibo
-
-# 시작값 a와 b, 그리고 피보나치 수열의 길이 n
-a = 0
-b = 1
 n = int(input())
+prev = 0
+curr = 1
+# result = fibonacci(n)
+for _ in range(2, n + 1):
+    prev, curr = curr, prev + curr
 
-result = fibo_fn(a, b, n)
 
-print( result[-1])
+print(curr)
