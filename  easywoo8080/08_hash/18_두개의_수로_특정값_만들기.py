896b55135466e5f98b3b9a68e10dc5d1531415a0
easywoo8080 <easywoo8080@gmail.com>
2024-12-11 09:05:11 +0900

    해시 드러가즈아

---
+++  easywoo8080/08_hash/18_두개의_수로_특정값_만들기.py
@@ -0,0 +1,26 @@
+import time
+
+test_case = []  # 전역 변수로 선언
+test_case.append([[1,2,3,4,8],6])
+test_case.append([[2,3,5,9],10])
+# test_case.append(27)
+# test_case.append(12345)
+
+class Problem:
+    '''
+    문제 내용
+    '''
+
+class Solution(Problem):
+
+    def solution(self, param): 
+        answer = []
+        print(param)
+        return answer
+
+# Object creation and method call (객체 생성 및 메서드 호출)
+sol = Solution()
+start_time = time.time()
+print( [sol.solution(t) for t in test_case] )
+elapsed_time = time.time() - start_time
+print(f"Time: {elapsed_time:.6f} seconds")
